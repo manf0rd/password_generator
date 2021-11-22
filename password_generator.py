4882c79bf9ee99b7fe5ed8e2074592637002fd2b
Jon <jonathan.k.estep@gmail.com>
2021-11-22 15:22:12 -0500

    rewrite

--- password_generator.py
+++ password_generator.py
@@ -1,124 +1,147 @@
-#! pyton3
-import os,random,string,shutil,time
-class file_mover:
-    def __init__(self, passwords ,filepath):
-        self.filepath = filepath
-        self.passwords = passwords
-        self.new_filepath = filepath
-        self.name = os.path.basename(filepath)
+#! python3
+#TODO add import csv support, Config.ini
+import os, random, shutil, subprocess, time
+from string import ascii_lowercase as lowercase_in_password, ascii_uppercase as uppercase_in_password, digits as digits_in_password
 
-    def move(self, new_filepath):
-        try:
-            if not os.path.exists(os.path.dirname(new_filepath)):
-                os.makedirs(os.path.dirname(new_filepath))
-            shutil.move(self.filepath, new_filepath)
-            print(f'File moved: {new_filepath}')
-        except OSError as error:
-            print(error)
-            print(f'Unable to move: {self.filepath}')
-            continue_prompt()
+def clip_text(text):
+	subprocess.run(['clip.exe'], input=text.strip().encode('utf-16'), check=True)
 
-    def delete(self):
-        try:
-            os.remove(self.filepath)
-            print(f'File deleted: {self.filepath}')
-        except OSError as error:
-            print(error)
-            print(f'{self.filepath} can not be removed')
-            continue_prompt()
+def build_password_generator_configuration(configuration_dict={}):
+	default_password_generator_configuration = {
+		'number_of_passwords' : 1, 'password_length' : 16, 'digits_in_password' : 2,
+		'uppercase_in_password' : 2, 'lowercase_in_password' : 2, 'specials_in_password' : 2
+		}
+	if configuration_dict.keys() != default_password_generator_configuration.keys():
+		configuration_dict = default_password_generator_configuration
+		
+	print('Current Password Generator Configuration:\n')
+	for item in configuration_dict:
+		print(f'{item.upper()} : {configuration_dict[item]}')
+	property_to_change = input(f'\nEnter property name to change or press Enter to continue with current configuration:\n')
 
-    def write(self,passwords):
-        try:
-            if os.path.exists(self.filepath):
-                os.remove(self.filepath)
-            with open(self.filepath, 'w') as file:
-                file.write('\n'.join(str(item) for item in passwords))
-                print(f'Passwords written to: {self.filepath}')
-        except OSError as error:
-            print(error)
-            print(f'Unable to write {self.filepath}')
-            continue_prompt()
+	if property_to_change != '' and property_to_change in configuration_dict.keys():
+		value = int(input(f'New value for {property_to_change}:\n'))		
+		if not value >= 1:
+			value = input(f'Value must be a number.  Type new value for {property_to_change} or press Enter to abort:\n')
+		else:
+			configuration_dict[property_to_change] = value
+			build_password_generator_configuration(configuration_dict)
 
-def generate_password(password_length=16):
-    password = ''
-    specials = ['!','@','$','%','^','&','*','<','>','?',';','~',]
-    while len(password) < password_length:
-        for i in range(2):
-            if len(password) < password_length: password += random.choice(string.ascii_lowercase)
-            if len(password) < password_length: password += random.choice(string.ascii_uppercase)
-            if len(password) < password_length: password += random.choice(string.digits)
-            if len(password) < password_length: password += random.choice(specials)
-    list_password = list(password)
-    random.shuffle(list_password)
-    new_password = ''.join(list_password)
-    return new_password
+	elif property_to_change != '':
+		print(f'{property_to_change} is not a valid property\nPlease ensure that what you type matches one of the following:\n')
+		for i in configuration_dict.keys:
+			print(i)
+		build_password_generator_configuration(default_password_generator_configuration)
 
-def continue_prompt():
-    answer = input(f'Would you like to continue? Yes or No?\n')
-    answer = answer[:1]
-    if answer == 'y':
-        main()
-    else:
-        exit()
+	sum_of_complexity = int(configuration_dict['digits_in_password']) + int(configuration_dict['uppercase_in_password']) + int(configuration_dict['lowercase_in_password']) + int(configuration_dict['specials_in_password'])
+	if sum_of_complexity > configuration_dict['password_length']:
+		print('Invalid configuration:\nNumber of required characters is greater than password length\n')
+		build_password_generator_configuration(default_password_generator_configuration)
+		return build_password_generator_configuration(configuration_dict)
+	return configuration_dict
 
-def clear_console():
-    os.system('cls' if os.name=='nt' else 'clear')
+def password_generator(password_configuration_dict):
+	new_password = ''
+	specials_in_password = ['!','@','$','%','^','&','*','<','>','?',';','~',]
+	password_complexity = [uppercase_in_password, lowercase_in_password, digits_in_password, specials_in_password]
+	while len(new_password) < password_configuration_dict['password_length']:
+		for item in password_complexity:
+			if len(new_password) < password_configuration_dict['password_length']:
+				new_password += random.choice(item)
+	return new_password	
 
-def exist_menu(root_path,password_file):
-    choice = ''
-    while choice[:1].lower() not in ['a','o']:
-        choice = input('Password file already exists.  (A)rchive existing first or just (O)verwrite?\n')
-    match choice:
-        case 'a': #archive file
-            time_string = time.strftime('%H-%M-%S')
-            folder_name = os.path.join(root_path,time.strftime('%Y-%m-%d'))
-            new_filepath = os.path.join(folder_name, f'{time_string}_passwords.txt')
-            password_file.move(new_filepath)
-            password_file.write(password_file.passwords)
-            continue_prompt()
-        case 'o': #overwrite file
-            password_file.write(password_file.passwords)
-            continue_prompt()
+def save_file(passwords):
+	filepath = 'c:\\Temp\\Password_Generator\\passwords.txt'
+	change_name = yes_no_prompt(f'Default save location is: {filepath}\nWould you like to change it?')
+	if change_name:
+		passworld_file = input('Enter new file path to save to: (c:\\temp\\passwords.txt)\n')
+	else:
+		password_file = filepath
+
+	try:
+		if os.path.exists(password_file):
+			archive_file = yes_no_prompt('File already exists. Would you like to archive before saving?')
+			if archive_file:
+				time_string = time.strftime('%H-%M-%S')
+				folder_name = os.path.join(os.path.dirname(password_file),time.strftime('%Y-%m-%d'))
+				old_password_file = os.path.join(folder_name, f'{time_string}_passwords.txt')
+				try:
+					if not os.path.exists(folder_name):
+						try:
+							os.makedirs(folder_name)
+						except OSError as error:
+							print(error)
+							print('Unable to create archive directory')
+					shutil.move(password_file, old_password_file)
+					print(f'Archive file moved:{old_password_file}')
+					new_file = open(password_file, 'x')
+					new_file.write(passwords)
+					new_file.close()
+					print(f'Created new file: {password_file}')
+				except OSError as error:
+					print(error)
+					print('Failed to archive')
+			else:
+				try:
+					file = open(password_file, 'w')
+					file.write(passwords)
+					file.close()
+					print(f'File overwritten: {password_file}')
+				except OSError as error:
+					print(error)
+					print('Failed to overwrite')
+		else:
+			if not os.path.exists(os.path.dirname(password_file)):
+				try:
+					os.makedirs(os.path.dirname(password_file))
+					file = open(password_file, 'x')
+					file.write(passwords)
+					file.close()
+					print(f'File and directories created: {password_file}')
+				except OSError as error:
+					print(error)
+					print('Failed to write new file')
+			else:
+				try:
+					file = open(password_file, 'x')
+					file.write(passwords)
+					file.close()
+					print(f'New file created: {password_file}')
+				except OSError as error:
+					print(error)
+					print('Failed to write new file')
+	except OSError as error:
+		print(error)
+		print('Failed to save')
+
+def yes_no_prompt(option=''):
+	to_continue = 'Would you like to continue?'
+	answer = input(f'{option if option else to_continue}\n')
+	return True if answer[:1].lower() == 'y' else False
 
 def main():
-    clear_console()
-    root_path = 'c:\\temp\\Password_Generator'
-    path = input(f'Enter a new export location or press Enter to contine with default - {root_path}\n')
-    confirm = ''
-    while confirm[:1].lower() != 'y':
-        confirm = input(f'Passwords will be written as {os.path.join(path, "passwords.txt")}\nIs this correct? (Y)es or (N)o?\n')
-        if confirm[:1].lower() == 'n':
-            root_path = 'c:\\temp\\Password_Generator'
-            path = input(f'Enter a new export location or press Enter to contine with default - {root_path}\n')
-    if not path == '': root_path = path
-    while not os.path.isdir(root_path):
-        try:
-            os.makedirs(root_path)
-        except OSError as error:
-            print(error)
-            print('Please ensure path is entered correctly. Example: c:\\temp\passwords')        
-    
-    number = input('Enter number of passwords to generate:\n')
-    while not number.isdigit():
-        number = input('Enter number of passwords to generate:\n')
+	password_configuration = build_password_generator_configuration()	
+	if password_configuration['number_of_passwords'] > 1:
+		password_list = ''
+		for i in range(password_configuration['number_of_passwords']):
+			password_list += f'{password_generator(password_configuration)}\n'		
+		print(f'New Passwords:\n\n{password_list}')
+		print('\nPasswords also copied to clipboard')
+		clip_text(password_list)
+		choice = yes_no_prompt('Would you like to save the passwords to file?')
+		if choice:
+			save_file(password_list)
 
-    password_length = input('Enter password Length:\n')
-    while not password_length.isdigit():
-        password_length = input('Please provide a number of passwords to generate:\n')
-    passwords = [] 
+	else:
+		new_password = password_generator(password_configuration)
+		print(f'New Password\n{new_password}')
+		print('\nPassword also copied to clipboard')
+		clip_text(new_password)
+		choice = yes_no_prompt('Would you like to save the passwords to file?')
+		if choice:
+			save_file(new_password)
 
-    for i in range(int(number)):
-        password = generate_password(int(password_length))
-        passwords.append(password)
-    
-    password_file_path = os.path.join(root_path, 'passwords.txt')
-    password_file = file_mover(passwords, password_file_path)
-
-    if os.path.exists(password_file.filepath):
-        exist_menu(root_path, password_file)
-    else:
-        password_file.write(passwords)
-    continue_prompt()
+	main() if yes_no_prompt() else exit()
 
 if __name__ == '__main__':
-    main()
+	main()
